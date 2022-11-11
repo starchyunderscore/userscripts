f29bc9aefb0d12bf915cc86616f671760ef38a11
starchyunderscore <102399926+starchyunderscore@users.noreply.github.com>
2022-11-11 18:07:06 -0500

    Update bluecheckforzvaigzdelasas.js

--- scripts/bluecheckforzvaigzdelasas.js
+++ scripts/bluecheckforzvaigzdelasas.js
@@ -12,7 +12,7 @@ var elms = document.getElementsByTagName("a");
 for (var i = 0; i < elms.length; i++) {
   var e = elms[i];
   // alert(e.innerHTML);
-  if (e.innerHTML==`zvaigzdelasas`||e.innerHTML==`@<!-- -->zvaigzdelasas`||e.innerHTML==`<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" role="presentation"><use href="#managed-icon__earth"></use></svg> <!-- -->zvaigzdelasas.tumblr.com`||e.innerHTML==`<div class="sqHC2">zvaigzdelasas</div>`) {
+  if (e.innerHTML=='zvaigzdelasas'||e.innerHTML=='@<!-- -->zvaigzdelasas'||e.innerHTML=='<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" role="presentation"><use href="#managed-icon__earth"></use></svg> <!-- -->zvaigzdelasas.tumblr.com'||e.innerHTML=='<div class="sqHC2">zvaigzdelasas</div>') {
     e.innerHTML+='<svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" class="hlLCc" role="presentation"><use href="#managed-icon__blue-checkmark"></use></svg>';
   }
 }
