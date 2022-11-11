a6a684d945bbf823ec70e9d7c90835b0032f7992
starchyunderscore <102399926+starchyunderscore@users.noreply.github.com>
2022-11-11 17:47:46 -0500

    Create bluecheckforzvaigzdelasas.js

---
+++ scripts/bluecheckforzvaigzdelasas.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        New script - tumblr.com
+// @namespace   Violentmonkey Scripts
+// @match       https://www.tumblr.com/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 11/11/2022, 5:08:45 PM
+// ==/UserScript==
+
+var elms = document.getElementsByTagName("a");
+for (var i = 0; i < elms.length; i++) {
+  var e = elms[i];
+  if (e.innerHTML==`zvaigzdelasas`) {
+    e.innerHTML+='<svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" class="hlLCc" role="presentation"><use href="#managed-icon__blue-checkmark"></use></svg>';
+  }
+}
