42857e79b765457b365ccd25a4f24631c6fc81c2
starchyunderscore <102399926+starchyunderscore@users.noreply.github.com>
2022-11-12 01:29:04 -0500

    Update bluecheckforall.js
    I LOVE YOU STACKOVERFLOW
    
    https://stackoverflow.com/questions/2844565/is-there-a-javascript-jquery-dom-change-listener

--- scripts/bluecheckforall.js
+++ scripts/bluecheckforall.js
@@ -9,13 +9,28 @@
 // @description 2022-11-11, 5:08:45 PM
 // ==/UserScript==
 test();
-window.onscroll = function() {test()};
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+var observer = new MutationObserver(function(mutations, observer) {
+    console.log(mutations, observer);
+    test();
+});
+observer.observe(document, {
+  subtree: true,
+  attributes: true
+});
 function test(){
-  var elms = document.getElementsByClassName("sqHC2")
+  var elms = document.getElementsByClassName("sqHC2");
+  var elms2 = document.getElementsByClassName("BSUG4");
   for (var i = 0; i < elms.length; i++) {
     var e = elms[i];
     e.innerHTML+='<svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" class="hlLCc" role="presentation"><use href="#managed-icon__blue-checkmark"></use></svg><svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" class="hlLCc" role="presentation"><use href="#managed-icon__blue-checkmark"></use></svg>';
     e.className="added_checks";
   }
-  console.log("1");
+  for (var i = 0; i < elms2.length; i++) {
+    var e = elms2[i];
+    if(e.children.length==0){
+      e.innerHTML+='<svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" class="hlLCc" role="presentation"><use href="#managed-icon__blue-checkmark"></use></svg><svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" class="hlLCc" role="presentation"><use href="#managed-icon__blue-checkmark"></use></svg>';
+    }
+    e.className="added_checks";
+  }
 }
