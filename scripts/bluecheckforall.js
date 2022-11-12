23bf0a6a18dcd952b6d7d2d18490ec30cf644ae1
starchyunderscore <102399926+starchyunderscore@users.noreply.github.com>
2022-11-12 10:09:19 -0500

    Update bluecheckforall.js
    Thank you https://www.reddit.com/user/DeeSnow97/

--- scripts/bluecheckforall.js
+++ scripts/bluecheckforall.js
@@ -4,33 +4,48 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.tumblr.com/*
 // @grant       none
-// @version     2.1
-// @author      starchyunderscore
+// @version     2.2
+// @author      starchyunderscore && https://www.reddit.com/user/DeeSnow97/
 // @description 2022-11-11, 5:08:45 PM
 // ==/UserScript==
-test();
+
+let updateOnNextFrame = false;
+
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 var observer = new MutationObserver(function(mutations, observer) {
-    console.log(mutations, observer);
-    test();
+  updateOnNextFrame = true;
 });
 observer.observe(document, {
   subtree: true,
   attributes: true
 });
-function test(){
-  var elms = document.getElementsByClassName("sqHC2");
-  var elms2 = document.getElementsByClassName("BSUG4");
-  for (var i = 0; i < elms.length; i++) {
-    var e = elms[i];
-    e.innerHTML+='<svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" class="hlLCc" role="presentation"><use href="#managed-icon__blue-checkmark"></use></svg><svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" class="hlLCc" role="presentation"><use href="#managed-icon__blue-checkmark"></use></svg>';
-    e.className="added_checks";
+
+function updateCheckmarks(){
+  const checkmark='<svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" class="hlLCc" style="margin-left: 3px;" role="presentation"><use href="#managed-icon__blue-checkmark"></use></svg><svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" class="hlLCc" style="margin-left: 3px;" role="presentation"><use href="#managed-icon__blue-checkmark"></use></svg>';
+  const mainBlogLinks = document.querySelectorAll('div[aria-label="avatar"] + div a > div:not(.added_checks)');
+  const secondaryBlogLinks = document.querySelectorAll('header[role="banner"] a:not(.added_checks)');
+
+  for (var i = 0; i < mainBlogLinks.length; i++) {
+    var currentElm = mainBlogLinks[i];
+    currentElm.innerHTML+=checkmark;
+    currentElm.classList.add("added_checks");
   }
-  for (var i = 0; i < elms2.length; i++) {
-    var e = elms2[i];
-    if(e.children.length==0){
-      e.innerHTML+='<svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" class="hlLCc" role="presentation"><use href="#managed-icon__blue-checkmark"></use></svg><svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" class="hlLCc" role="presentation"><use href="#managed-icon__blue-checkmark"></use></svg>';
+  for (var i = 0; i < secondaryBlogLinks.length; i++) {
+    var currentElm = secondaryBlogLinks[i];
+    if(currentElm.children.length==0){
+      currentElm.innerHTML+=checkmark;
     }
-    e.className="added_checks";
+    currentElm.classList.add("added_checks");
   }
 }
+
+function loop(){
+  requestAnimationFrame(loop);
+
+  if(updateOnNextFrame){
+    updateOnNextFrame = false;
+    updateCheckmarks();
+  }
+}
+
+loop();
