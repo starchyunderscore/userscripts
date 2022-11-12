2cef7834bcfe9c81bdcd9c82fb178c54c0cf4636
starchyunderscore <102399926+starchyunderscore@users.noreply.github.com>
2022-11-12 09:16:54 -0500

    Update bluecheckforzvaigzdelasas.js

--- scripts/bluecheckforzvaigzdelasas.js
+++ scripts/bluecheckforzvaigzdelasas.js
@@ -1,21 +1,37 @@
 // ==UserScript==
-// @name        bluecheckforzvaigzdelasas - tumblr.com
+// @name        bluecheckforall - tumblr.com
+// @frames
 // @namespace   Violentmonkey Scripts
 // @match       https://www.tumblr.com/*
 // @grant       none
-// @version     1.6
+// @version     2.2
 // @author      starchyunderscore
 // @description 2022-11-11, 5:08:45 PM
 // ==/UserScript==
-test();
-window.onscroll = function() {test()};
-function test(){
-  var elms = document.getElementsByClassName("sqHC2")
-  for (var i = 0; i < elms.length; i++) {
-    var e = elms[i];
-    if (e.innerHTML=='zvaigzdelasas') {
-      e.innerHTML+='<svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" class="hlLCc" role="presentation"><use href="#managed-icon__blue-checkmark"></use></svg>';
+MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+var observer = new MutationObserver(function(mutations, observer) {
+  updateCheckmarks();
+});
+observer.observe(document, {
+  subtree: true,
+  attributes: true
+});
+function updateCheckmarks(){
+  const checkmark='<svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" class="hlLCc" role="presentation"><use href="#managed-icon__blue-checkmark"></use></svg><svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" class="hlLCc" role="presentation"><use href="#managed-icon__blue-checkmark"></use></svg>';
+  // var mainBlogLinks = document.querySelectorAll('div[aria-label="avatar"] + div a > div')
+  // var secondaryBlogLinks = document.querySelectorAll('header[role="banner"] a')
+  var mainBlogLinks = document.getElementsByClassName("sqHC2");
+  var secondaryBlogLinks = document.getElementsByClassName("BSUG4");
+  for (var i = 0; i < mainBlogLinks.length; i++) {
+    var currentElm = mainBlogLinks[i];
+    currentElm.innerHTML+=checkmark;
+    currentElm.className="added_checks";
+  }
+  for (var i = 0; i < secondaryBlogLinks.length; i++) {
+    var currentElm = secondaryBlogLinks[i];
+    if(currentElm.children.length==0){
+      currentElm.innerHTML+=checkmark;
     }
-    e.className="itterated";
+    currentElm.className="added_checks";
   }
 }
