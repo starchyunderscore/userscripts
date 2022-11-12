906b36375e1fbd02d21ae8e1548d38c1fcfe024a
starchyunderscore <102399926+starchyunderscore@users.noreply.github.com>
2022-11-12 00:40:51 -0500

    Update bluecheckforzvaigzdelasas.js

--- scripts/bluecheckforzvaigzdelasas.js
+++ scripts/bluecheckforzvaigzdelasas.js
@@ -3,13 +3,19 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://www.tumblr.com/*
 // @grant       none
-// @version     1.4
+// @version     1.3
 // @author      starchyunderscore
 // @description 2022-11-11, 5:08:45 PM
 // ==/UserScript==
-
-var elms = document.getElementsByClassName("sqHC2")
-for (var i = 0; i < elms.length; i++) {
-  var e = elms[i];
-  e.innerHTML+='<svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" class="hlLCc" role="presentation"><use href="#managed-icon__blue-checkmark"></use></svg>';
+test();
+window.onscroll = function() {test()};
+function test(){
+  var elms = document.getElementsByClassName("sqHC2")
+  for (var i = 0; i < elms.length; i++) {
+    var e = elms[i];
+    if (e.innerHTML=='zvaigzdelasas') {
+      e.innerHTML+='<svg xmlns="http://www.w3.org/2000/svg" height="14" width="14" class="hlLCc" role="presentation"><use href="#managed-icon__blue-checkmark"></use></svg>';
+    }
+    e.className="itterated";
+  }
 }
