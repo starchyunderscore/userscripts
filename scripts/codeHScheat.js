56b791fdba6156d4eb6050ccac661b2bd18c385a
starchyunderscore <spenceramay22@gmail.com>
2023-11-02 10:31:04 -0400

    Update codeHScheat.js

--- scripts/codeHScheat.js
+++ scripts/codeHScheat.js
@@ -15,14 +15,30 @@ const EVENTS = {
     INPUT: new Event("input", EVENT_OPTIONS),
 };
 
+function toHex(s) {
+    var s = unescape(encodeURIComponent(s))
+    var h = ''
+    for (var i = 0; i < s.length; i++) {
+        h += s.charCodeAt(i).toString(16)
+    }
+    return h
+}
+
+function fromHex(h) {
+    var s = ''
+    for (var i = 0; i < h.length; i+=2) {
+        s += String.fromCharCode(parseInt(h.substr(i, 2), 16))
+    }
+    return decodeURIComponent(escape(s))
+}
+
 window.addEventListener("load", (event) => {
   const BUTTON_ONE = document.getElementsByClassName("__abacus_button")[2] // Submit & continue or next
   if(BUTTON_ONE.innerHTML == "Next") { // skip if it is an example
     BUTTON_ONE.click();
   } else if (BUTTON_ONE.innerHTML == "Submit + Continue") { // put code if it is an example
     const inputElement = document.querySelector("#ace_text-input-textarea");
-    const assignment = document.getElementsByClassName("__abacus_editor-label")[0].children[0].innerHTML
-    
+    const assignment = toHex(document.getElementsByClassName("__abacus_editor-label")[0].children[0].innerHTML);
     inputElement.value = "5454545454545454";
     const tracker = inputElement._valueTracker;
     tracker && tracker.setValue("5454545454545454");
